82f464d4afb7b389cfe0d0f4d54d14c5f7058b25
Hakan Kayra Doğan <hkayrad@gmail.com>
2023-08-10 15:40:02 +0300

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import "./style/index.css";
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+//* Routes
+import Home from './routes/Home';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
